08f70591b6a8c6e5c5fb7832d6cf63e89dc1c445
Tobias <githubst@gmail.com>
2018-06-26 22:09:29 +0200

    String Template

---
+++ String Template/ES5StringTemplate.js
@@ -0,0 +1,12 @@
+var animals = {
+  Lion: {
+    Family: "Felidae"
+  },
+  Turtle: {
+    Family: "Reptile"
+  }
+};
+
+var infos = "Family of Lion :  " + animals.Lion.Family + ",\n  Family of Turtle: " + animals.Turtle.Family;
+
+console.log(infos);
---
+++ String Template/ES6StringTemplate.js
@@ -0,0 +1,13 @@
+const animals = {
+	Lion:  {
+    	Family: "Felidae"
+    },
+  	Turtle: {
+		Family: "Reptile"
+	}
+}
+
+const infos = `Family of Lion :  ${animals.Lion.Family},
+  Family of Turtle: ${animals.Turtle.Family}`
+
+console.log(infos)
